dae14d5ca5e65a59f2e35f8ac1cef97eb9336763
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2025-07-07 18:56:39 +0530

    Added About Section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,41 @@
+import countryFacts from "../api/countryData.json";
 import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <section className="section-about container">
+      <h2 className="container-title">
+        Here are the Interesting Facts
+        <br />
+        we're proud of
+      </h2>
+
+      <div className="gradient-cards">
+        {countryFacts.map((country) => {
+          const { id, countryName, capital, population, interestingFact } =
+            country;
+          return (
+            <div className="card" key={id}>
+              <div className="container-card bg-blue-box">
+                <p className="card-title">{countryName}</p>
+                <p>
+                  <span className="card-description">Capital:</span>
+                  {capital}
+                </p>
+                <p>
+                  <span className="card-description">Population:</span>
+                  {population}
+                </p>
+                <p>
+                  <span className="card-description">Interesting Facts:</span>
+                  {interestingFact}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
